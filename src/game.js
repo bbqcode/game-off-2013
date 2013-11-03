91de1bbac155f083aa9eb36de9d830b978698916
Martin Fournier <martinfournier@scolab.com>
2013-11-03 09:41:02 -0500

    Tests

--- src/game.js
+++ src/game.js
@@ -4,6 +4,20 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, 
 var cactuar;
 var cursors;
 
+var map = 
+[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ,
+    [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0] ,
+    [0, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0] ,
+    [0, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0]
+]
+
+var rectangles = [];
 function preload() {
     //  You can fill the preloader with as many assets as your game requires
     game.stage.backgroundColor = '#007236';
@@ -21,7 +35,7 @@ function preload() {
 function create() {
     //  This creates a simple sprite that is using our loaded image and
     //  displays it on-screen
-    cactuar = game.add.sprite(0, 0, 'cactuar');
+    cactuar = game.add.sprite(0, 400, 'cactuar');
     game.world.setBounds(-1000, -1000, 2000, 2000);
 
     //  This will create a new object called "cursors", inside it will contain 4 objects: up, down, left and right.
@@ -30,8 +44,26 @@ function create() {
 
 
     //constant x velocity
-    cactuar.body.velocity.x = 50;
+    cactuar.body.velocity.x = 100;
 
+    var tileSize = 18;
+    for (var x = 0; x < map.length; x++) {
+        for (var y = 0; y < map[x].length; y++) {
+            var rect = new Phaser.Rectangle(y * tileSize,x * tileSize, tileSize, tileSize);
+            switch (map[x][y]) {
+                case 0:
+                    rect.color = '#000000';
+                    break;
+                case 1:
+                    rect.color = '#00ff00';
+                    break;
+                case 2:
+                    rect.color = '#0000ff';
+                    break;
+            }
+            rectangles.push(rect);
+        }
+    }
 }
 
 function update() {
@@ -45,11 +77,18 @@ function update() {
     } else if (cursors.up.isDown) {
         cactuar.y -= 4;
     }
+
+    if (cactuar.x > 800) {
+        cactuar.x = 0;
+    }
 }
 
 function render() {
 
-    game.debug.renderCameraInfo(game.camera, 32, 32);
+    game.debug.renderCameraInfo(game.camera, 302, 32);
     game.debug.renderSpriteInfo(cactuar, 32, 200);
-
+    for (var x = 0; x < rectangles.length; x++) {
+        var r = rectangles[x];
+        game.debug.renderRectangle(r, r.color);
+    }
 }
